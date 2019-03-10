a7e2b0f34eb81bf65b7843c5f997681fed0a2bc4
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-11 01:04:23 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@
  * Check if a string is a [valid filename](https://github.com/sindresorhus/filename-reserved-regex).
  *
  * @param input - The string to check for whether it is a valid filename.
- * @returns A `boolean` of whether `input` is a valid filename or not.
+ * @returns Whether `input` is a valid filename.
  */
 export default function validFilename(input: string): boolean;
